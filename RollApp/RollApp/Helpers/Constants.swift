ddd79f07b4d308e691a110717d58f7742ed91206
JulyBelova <july@belova.es>
2024-11-06 17:23:39 +0300

    added launch, logoView, constants

---
+++ RollApp/RollApp/Helpers/Constants.swift
@@ -0,0 +1,48 @@
+//
+//  Constants.swift
+//  RollApp
+//
+//  Created by July Belova on 06.11.2024.
+//
+import Foundation
+
+enum ConstantsOnboarding {
+    static let topLabel = "Роллы, которые доставляют удовоствие!"
+}
+enum ConstantsAuthVC {
+    static let loginTF = "Enter your login"
+    static let passwordTF = "Enter your password"
+    static let forgotPassLabel = "Forgot Password?"
+    static let myButton = "Eat Away!"
+    static let signUpLabel = "Sign Up"
+}
+enum ConstantsSheetVC {
+    static let signInLabel = "Sign In"
+    static let nameTF = "Name"
+    static let loginTF = "Email"
+    static let passwordTF = "Password"
+    static let conformPasswordTF = "Conform Password"
+    static let myButton = "Never Hungry Again!"
+}
+enum ConstantsNewPass {
+    static let topLabel = "Your password must be at-least 8 characters long."
+    static let newPassTF = "Enter new password"
+    static let passwordTF = "Reconfirm new password"
+    static let myButton = "Eat Away!"
+}
+enum ConstantsAlert {
+    static let titleAlertLabel = "Forgot Password"
+    static let titleLabel = "Verification Code"
+    static let smallTitleLabel = "A verification code has been sent to your mail"
+    static let emailTextField = "Enter your account email"
+    static let resetButton = "Reset Password"
+    static let changeButton = "Change Email"
+}
+enum ImageName {
+    static let logo = "icon"
+}
+enum SystemImageName {
+    static let arrowRight = "arrow.right"
+    static let chevronUp = "chevron.up"
+}
+
